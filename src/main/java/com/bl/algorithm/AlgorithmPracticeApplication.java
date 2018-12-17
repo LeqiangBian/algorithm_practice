0b013059d53a772ee86fdf89fa833d735834f369
bianleqiang <bianleqiang@xdf.cn>
2018-12-17 13:42:52 +0800

    [frist commit , spring boot]

---
+++ src/main/java/com/bl/algorithm/AlgorithmPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.bl.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmPracticeApplication.class, args);
+    }
+
+}
+
